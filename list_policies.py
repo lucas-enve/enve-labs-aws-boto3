964b10c0ee8c8439fa3b278b225de5cf946d1900
lucas <lucas@MacBook-Air-de-lucas.local>
2025-05-20 17:38:13 -0300

    MOD: list_policies.py

--- list_policies.py
+++ list_policies.py
@@ -4,19 +4,21 @@ import csv
 client = boto3.client("iam")
 
 paginator = client.get_paginator("list_policies")
-pages = paginator.paginate(Scope='Local')  # Scope: Local/AWS/All
+pages = paginator.paginate(Scope='ALL')  # Scope: Local/AWS/All
 
-with open("list_local_policies.csv", "w", newline="") as f:
+with open("list_attached_policies.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(['PolicyName', 'PolicyId', 'Arn', 'CreateDate'])
+    writer.writerow(['PolicyName', 'PolicyId', 'Arn', 'AttachmentCount', 'CreateDate']) # ADD :
 
     for page in pages:
         for policy in page['Policies']:
-            writer.writerow([
-                policy['PolicyName'],
-                policy['PolicyId'],
-                policy['Arn'],
-                policy['CreateDate']
-            ])
+            if policy['AttachmentCount'] > 0:
+                writer.writerow([
+                    policy['PolicyName'],
+                    policy['PolicyId'],
+                    policy['Arn'],
+                    policy['AttachmentCount'],
+                    policy['CreateDate']
+                ])
 
-print("saved list_local_policies.csv")
+print("saved list_attached_policies.csv")
